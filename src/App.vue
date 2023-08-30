4b8acc62c786dcdf35d53b06e26bfea4108c9de9
sing <sing2536@gmail.com>
2023-08-31 08:08:42 +1000

    Removed background color from main page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <script setup>
-import Iframes from './components/Iframes.vue';
-import Nav from './components/Nav.vue'
-import ShareModal from './components/ShareModal.vue';
+import Iframes from "./components/Iframes.vue"
+import Nav from "./components/Nav.vue"
+import ShareModal from "./components/ShareModal.vue"
 </script>
 
 <template>
-    <Nav/>
-    <Iframes/>
-    <ShareModal/>
+    <Nav />
+    <Iframes />
+    <ShareModal />
 </template>
-
-<style scoped>
-
-</style>
