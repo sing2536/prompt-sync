b780095b52f2024963f8e1623cb8e01e482c4682
Sing <d832819@gmail.com>
2023-09-16 15:11:36 +1000

    Added shortcut to focus prompt in content script

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,47 +1,67 @@
-const isBard = (() => location.host == 'bard.google.com')()
-const isChatGPT = (() => location.host == 'chat.openai.com')()
+const isBard = (() => location.host == "bard.google.com")()
+const isChatGPT = (() => location.host == "chat.openai.com")()
 
 function query(data) {
     if (isChatGPT) {
         setReactValue(document.querySelector('[id="prompt-textarea"]'), data)
-        document.querySelector('[id="prompt-textarea"]').nextElementSibling.click()
+        document
+            .querySelector('[id="prompt-textarea"]')
+            .nextElementSibling.click()
     }
 
     if (isBard) {
-        const bard = document.querySelector('[aria-label="Input for prompt text"]')
+        const bard = document.querySelector(
+            '[aria-label="Input for prompt text"]'
+        )
         bard.value = data
-        bard.dispatchEvent(new Event('input'))
+        bard.dispatchEvent(new Event("input"))
         document.querySelector('[mattooltip="Submit"]').click()
     }
 }
 
-window.addEventListener("load", async ()=> {
-    if (isBard) {
-        while (document.querySelector('[aria-label="Input for prompt text"]') === null) {
-            await new Promise(resolve => requestAnimationFrame(resolve));
+window.addEventListener(
+    "load",
+    async () => {
+        if (isBard) {
+            while (
+                document.querySelector(
+                    '[aria-label="Input for prompt text"]'
+                ) === null
+            ) {
+                await new Promise((resolve) => requestAnimationFrame(resolve))
+            }
+            focusPrompt()
         }
-        chrome.runtime.sendMessage('', {action: 'focusPrompt'})
-    }
-}, false)
+    },
+    false
+)
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "/") focusPrompt()
+})
 
 chrome.runtime.onMessage.addListener((msg) => {
     switch (msg?.action) {
-        case 'query':
+        case "query":
             query(msg.data)
-            break;
+            break
     }
-});
+})
 
 function setReactValue(element, value) {
-    let lastValue = element.value;
-    element.value = value;
-    let event = new Event("input", { target: element, bubbles: true });
+    let lastValue = element.value
+    element.value = value
+    let event = new Event("input", { target: element, bubbles: true })
     // React 15
-    event.simulated = true;
+    event.simulated = true
     // React 16
-    let tracker = element._valueTracker;
+    let tracker = element._valueTracker
     if (tracker) {
-        tracker.setValue(lastValue);
+        tracker.setValue(lastValue)
     }
-    element.dispatchEvent(event);
+    element.dispatchEvent(event)
+}
+
+function focusPrompt() {
+    chrome.runtime.sendMessage("", { action: "focusPrompt" })
 }
