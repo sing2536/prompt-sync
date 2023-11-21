8050e65bbc069ad0b361a1834d41b520e23bd444
Sing Ridley-lee <sing2536@gmail.com>
2023-11-21 10:49:23 +1000

    implemeted toggle switches to chat setting

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -11,14 +11,15 @@ const { websites } = storeToRefs(settings)
     <Modal :modal-active="settings.active" @close="settings.close()">
         <div>
             <h2>Settings</h2>
-            <p>Websites Enabled</p>
+            <p>Chats enabled</p>
             <div class="checkboxContainer" v-for="website in websites">
+                <label :for="website.name">{{ website.name }}</label>
                 <input
+                    class="toggle"
                     :id="website.name"
                     v-model="website.active"
                     type="checkbox"
                 />
-                <label :for="website.name">{{ website.name }}</label>
             </div>
         </div>
     </Modal>
@@ -27,9 +28,60 @@ const { websites } = storeToRefs(settings)
 <style lang="less">
 .checkboxContainer {
     display: flex;
-    gap: 10px;
+    gap: 30px;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 8px;
 }
 label {
-    font-size: 12px;
+    // font-size: 12px;
+}
+
+.toggle {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    width: 36px;
+    height: 22px;
+    display: inline-block;
+    position: relative;
+    border-radius: 50px;
+    overflow: hidden;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    background-color: var(--color-text2);
+    transition: background-color ease 0.3s;
+}
+
+.toggle:before {
+    content: "";
+    display: block;
+    position: absolute;
+    z-index: 2;
+    width: 18px;
+    height: 18px;
+    background: #fff;
+    left: 2px;
+    top: 2px;
+    border-radius: 50%;
+    font: 10px/28px Helvetica;
+    text-transform: uppercase;
+    font-weight: bold;
+    text-indent: -22px;
+    word-spacing: 37px;
+    color: #fff;
+    text-shadow: -1px -1px rgba(0, 0, 0, 0.15);
+    white-space: nowrap;
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
+    transition: all cubic-bezier(0.3, 1.5, 0.7, 1) 0.3s;
+}
+
+.toggle:checked {
+    background-color: var(--color-primary);
+}
+
+.toggle:checked:before {
+    left: 16px;
 }
 </style>
