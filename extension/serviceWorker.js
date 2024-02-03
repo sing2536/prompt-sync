2811bd2ded889ddeeecb2ed0baf87347f488db29
Sing Ridley-lee <sing2536@gmail.com>
2024-02-03 10:16:33 +1000

    Fixed issue with chatGPT page not loading anymore

--- extension/serviceWorker.js
+++ extension/serviceWorker.js
@@ -1,21 +1,53 @@
-chrome.action.onClicked.addListener(()=> chrome.tabs.create({url: "app/index.html"}))
+chrome.action.onClicked.addListener(() =>
+    chrome.tabs.create({ url: "app/index.html" })
+)
 
 chrome.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: [1],
+    removeRuleIds: [1, 2],
     addRules: [
         {
-            "id": 1,
-            "priority": 1,
-            "action": {
-                "type": "modifyHeaders",
-                "requestHeaders": [
-                    { "header": "sec-fetch-dest", "operation": "set", "value": "document" }
+            id: 1,
+            priority: 1,
+            action: {
+                type: "modifyHeaders",
+                requestHeaders: [
+                    {
+                        header: "sec-fetch-dest",
+                        operation: "set",
+                        value: "document",
+                    },
                 ],
-                "responseHeaders": [
-                    { "header": "X-Frame-Options", "operation": "remove" }
-                ]
+                responseHeaders: [
+                    { header: "X-Frame-Options", operation: "remove" },
+                ],
             },
-            "condition": {"urlFilter": "bard.google.com", "resourceTypes": ["sub_frame"], "initiatorDomains": [chrome.runtime.id]}
-        }
+            condition: {
+                urlFilter: "bard.google.com",
+                resourceTypes: ["sub_frame"],
+                initiatorDomains: [chrome.runtime.id],
+            },
+        },
+        {
+            id: 2,
+            priority: 1,
+            action: {
+                type: "modifyHeaders",
+                requestHeaders: [
+                    {
+                        header: "sec-fetch-dest",
+                        operation: "set",
+                        value: "document",
+                    },
+                ],
+                responseHeaders: [
+                    { header: "Content-Security-Policy", operation: "remove" },
+                ],
+            },
+            condition: {
+                urlFilter: "chat.openai.com",
+                resourceTypes: ["sub_frame"],
+                initiatorDomains: [chrome.runtime.id],
+            },
+        },
     ],
 })
