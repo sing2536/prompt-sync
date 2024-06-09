a760c9ded149ad9be6b79f11fa4bc8dd6fe692bb
Sing Ridley-lee <sing2536@gmail.com>
2024-06-09 13:29:42 +1000

    Added v-focus directive

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-import './assets/base.less'
+import "./assets/base.less"
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import tooltip from './mixins/tooltip-vue3'
+import { createApp } from "vue"
+import { createPinia } from "pinia"
+import App from "./App.vue"
+import tooltip from "./mixins/tooltip-vue3"
+import focus from "./mixins/vFocus"
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(tooltip)
+app.use(focus)
 
-app.mount('#app')
+app.mount("#app")
---
+++ src/mixins/vFocus.js
@@ -0,0 +1,7 @@
+export default {
+    install: (app, options) => {
+        app.directive("focus", {
+            mounted: (el) => el.focus(),
+        })
+    },
+}
